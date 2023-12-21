ddcf7c08e633dae1f4898f2fec560c0823e05d1a
Hasnain Zia <hasnainzia438@gmail.com>
2023-12-21 13:42:55 +0500

    connected to mongodb

--- api/index.js
+++ api/index.js
@@ -1,5 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017")
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(3000, () => {
