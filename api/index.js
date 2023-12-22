99a2c504c7bf946c200e1b0bd84791ee4a473ea4
Hasnain Zia <hasnainzia438@gmail.com>
2023-12-22 16:17:03 +0500

    added auth controller

--- api/index.js
+++ api/index.js
@@ -1,15 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017")
+  .connect("mongodb://127.0.0.1:27017/mern-auth-db")
   .then(() => {
     console.log("connected to db");
   })
   .catch((err) => {
     console.log(err);
   });
+
 const app = express();
+app.use(express.json());
+
+app.use("/app/user", userRoutes);
+app.use("/app/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server is Running");
